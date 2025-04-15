58fe83cad8399133b57a169396385c030513c4a0
Hải Tân <221124022310@due.udn.vn>
2025-04-15 18:06:43 +0700

    Hai commit v6

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { getCorsOptions } from './common/config/cors.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
 
   const port = configService.get<number>('PORT') || 5000;
-  const allowedOrigins =
-    configService.get<string>('CORS_ORIGIN')?.split(',') || [];
 
   app.setGlobalPrefix('restful-api');
   app.useGlobalPipes(
@@ -19,17 +18,7 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
-  app.enableCors({
-    origin: (origin, callback) => {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-    credentials: true,
-    methods: 'GET',
-  });
+  app.enableCors(getCorsOptions(configService));
 
   await app.listen(port);
 
