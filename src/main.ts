71aed12d842c28c9879938c2841cf05d51daf2e2
Hải Tân <221124022310@due.udn.vn>
2025-04-15 19:33:07 +0700

    Hai commit v7

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,14 @@ import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { getCorsOptions } from './common/config/cors.config';
 
-async function bootstrap() {
+const bootstrap = async () => {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
 
   const port = configService.get<number>('PORT') || 5000;
+  const apiPrefix = configService.get<string>('API_PREFIX') || '';
 
-  app.setGlobalPrefix('restful-api');
+  app.setGlobalPrefix(apiPrefix);
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
